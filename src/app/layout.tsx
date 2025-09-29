6ccb8adf09b88ade3556dc469b73ff4f288a1e6a
coldwindz <happyindia2005@gmail.com>
2025-09-29 22:35:35 +0530

    clean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
 import type { Metadata } from "next";
 import { Lato, Geist } from "next/font/google";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 
 /* Used Lato and Geist from google as it was available and set weight to 400 as it was mentioned in css file */
-const lato = Lato({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-lato",
-});
+// const lato = Lato({
+//   subsets: ["latin"],
+//   weight: "400",
+//   variable: "--font-lato",
+// });
 
-const giest = Geist({
-  subsets: ["latin"],
-  variable: "--font-giest",
-});
+// const giest = Geist({
+//   subsets: ["latin"],
+//   variable: "--font-giest",
+// });
 
 /* Defining custom font instead of @font-face */
-const larken = localFont({
-  src: [
-    { path: "../fonts/Larken-Bold.otf", weight: "700" },
-    { path: "../fonts/Larken-Regular.otf", weight: "400" },
-  ],
-  variable: "--font-larken",
-});
+// const larken = localFont({
+//   src: [
+//     { path: "../fonts/Larken-Bold.otf", weight: "700" },
+//     { path: "../fonts/Larken-Regular.otf", weight: "400" },
+//   ],
+//   variable: "--font-larken",
+// });
 
 export const metadata: Metadata = {
   title: "Ruhava",
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${larken.variable} ${giest.variable} ${lato.variable} antialiased`}
+        // className={`${larken.variable} ${giest.variable} ${lato.variable} antialiased`}
       >
         {/* Made a separate Header and Footer file in the components folder to separate logic */}
         {children}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,18 +16,18 @@ export default function Header() {
     <header className="justify-between items-center  border-b border-gold">
       <div id="br1"></div>
 
-      <h1 className="text-center font-Larken font-medium text-4xl tracking-wide py-[18px]">
+      <h1 className="text-center font-Larken font-medium text-4xl tracking-wide py-[24px]">
         RUHAVA
       </h1>
 
-      <nav className="text-center space-x-6 pb-[18px]">
+      <nav className="text-center space-x-6 pb-[24px]">
         {navItems.map((item, idx) => (
           <a
             key={item}
             href="#"
             id="navbtn"
             className={`hover:text-gold font-Lato text-xs font-semibold tracking-wide ${
-              item === "ABOUT RUHAVA" ? "pl-[50px] px-[10px]" : "px-[22px]"
+              item === "ABOUT RUHAVA" ? "pl-[40px] px-[10px]" : "px-[12px]"
             }`}
           >
             {item}
